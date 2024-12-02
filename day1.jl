b8825eaa0190d485fb5ee54ce34ac20450bd5040
Tom Kwong <tk3369@gmail.com>
2024-12-01 21:37:48 -0800

    sync

--- day1.jl
+++ day1.jl
@@ -5,7 +5,34 @@ function input()
     return x, y
 end
 
+x, y = input()
+
 part1(x, y) = sum(abs, x .- y)
 
 part2(x, y) = sum(value * count(==(value), y) for value in x)
 
+#= benchmark results:
+julia> @benchmark part1($x, $y)
+BenchmarkTools.Trial: 10000 samples with 258 evaluations.
+ Range (min … max):  296.349 ns … 36.911 μs  ┊ GC (min … max):  0.00% … 97.54%
+ Time  (median):     834.946 ns              ┊ GC (median):     0.00%
+ Time  (mean ± σ):     1.175 μs ±  1.534 μs  ┊ GC (mean ± σ):  33.36% ± 22.12%
+
+  ▅▁▂▃█▆▃                                                ▁▁▁▁  ▁
+  ████████▇▅▄▁▁▁▁▁▁▁▃▁▁▃▁▁▁▁▁▁▁▃▁▁▃▃▁▃▃▄▄▃▅▆▇▇▇▇▆▄▆▆▆▇████████ █
+  296 ns        Histogram: log(frequency) by time      7.14 μs <
+
+ Memory estimate: 7.94 KiB, allocs estimate: 3.
+
+julia> @benchmark part2($x, $y)
+BenchmarkTools.Trial: 10000 samples with 1 evaluation.
+ Range (min … max):  84.833 μs … 144.041 μs  ┊ GC (min … max): 0.00% … 0.00%
+ Time  (median):     86.542 μs               ┊ GC (median):    0.00%
+ Time  (mean ± σ):   86.400 μs ±   2.172 μs  ┊ GC (mean ± σ):  0.00% ± 0.00%
+
+  ▆▆▂            ▅█▄▁       ▁              ▂                   ▁
+  ████▆▄▃▂▂▄▄▃▃▃▄█████▇█▅▆▅▆██▄▆▆▆█▇▇▅▇▆▆▅▆██▇▅▆▅▅▄▃▄▂▄▃▄▂▆▄▆▅ █
+  84.8 μs       Histogram: log(frequency) by time      91.2 μs <
+
+ Memory estimate: 32 bytes, allocs estimate: 2.
+=#
--- day2.jl
+++ day2.jl
@@ -15,8 +15,36 @@ function is_safe_by_removing_one_level(levels)
     return is_safe(levels) || any(is_safe, combinations)
 end
 
-# part1
-sum(is_safe, input())
+data = input()
 
-# part2
-sum(is_safe_by_removing_one_level, input())
+part1(data) = sum(is_safe, data)
+
+part2(data) = sum(is_safe_by_removing_one_level, input())
+
+#= Benchmark results:
+
+julia> @benchmark part1($data)
+BenchmarkTools.Trial: 10000 samples with 1 evaluation.
+ Range (min … max):  25.750 μs …  15.895 ms  ┊ GC (min … max):  0.00% … 99.61%
+ Time  (median):     29.833 μs               ┊ GC (median):     0.00%
+ Time  (mean ± σ):   35.578 μs ± 246.633 μs  ┊ GC (mean ± σ):  14.57% ±  2.22%
+
+    ▇█▃       ▃
+  ▁▄███▄▂▂▂▂▃▅█▇▅▄▅▇▇▇█▇▆▅▆▅▄▄▄▅▄▃▃▃▃▃▂▂▃▂▂▂▂▂▂▂▂▂▂▂▂▂▂▁▁▁▁▁▁▁ ▃
+  25.8 μs         Histogram: frequency by time         39.2 μs <
+
+ Memory estimate: 101.44 KiB, allocs estimate: 2000.
+
+julia> @benchmark part2($data)
+BenchmarkTools.Trial: 5995 samples with 1 evaluation.
+ Range (min … max):  697.708 μs …  29.670 ms  ┊ GC (min … max):  0.00% … 96.96%
+ Time  (median):     722.042 μs               ┊ GC (median):     0.00%
+ Time  (mean ± σ):   832.755 μs ± 689.178 μs  ┊ GC (mean ± σ):  12.11% ± 14.16%
+
+  █▄▂▁                                                          ▁
+  ████▇▆▃▃▃▁▁▁▁▃▁▁▃▁▁▁▄▁▄▃▁▁▃▁▁▁▁▁▁▃▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▅█▇▇▅▁▄▄▅▇█ █
+  698 μs        Histogram: log(frequency) by time       3.64 ms <
+
+ Memory estimate: 2.06 MiB, allocs estimate: 38705.
+
+ =#
